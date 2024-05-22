2ad6d9722d5eeb2be2247921a43c58865439a67d
MohammadReza Ghalami <ghalami.mr@gmail.com>
2024-05-22 14:43:09 +0330

    crypto app completed

---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,18 @@
+import styles from "./Layout.module.css";
+
+function Layout({ children }) {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>Crypto App</h1>
+        <p>React.js Project</p>
+      </header>
+      {children}
+      <footer className={styles.footer}>
+        <p>Developed by Ghalami with ❤️</p>
+      </footer>
+    </>
+  );
+}
+
+export default Layout;
